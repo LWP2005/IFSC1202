c407bc2c7d970e8f0e44d8f77b5a27875a672980
LWP2005 <lwperry1@ualr.edu>
2024-09-16 14:54:57 +0000

    09/16/2024

---
+++ Assignments/04.Project.py
@@ -0,0 +1,4 @@
+start = int(input("Enter Start of Range: "))
+end = int(input("Enter End of Range: "))
+
+for i in range(start, end + 1):
---
+++ Section 05/05.01 Sum of Sequence.py
@@ -0,0 +1,7 @@
+sum = 0
+n = ("Enter a Number (CR to Quit): ")
+
+while n != 0:
+    int(input(n))
+    sum += sum
+print(sum)
